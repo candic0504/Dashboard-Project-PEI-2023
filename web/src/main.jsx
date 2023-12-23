4169c9aced031c9748150cb098c22bac3e83d5bf
Pmayaud <paul.mayaud@edu.ece.fr>
2023-12-23 15:39:21 +0100

    ajout BDD Paul

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 
 import Apid from "./Apid.jsx";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
