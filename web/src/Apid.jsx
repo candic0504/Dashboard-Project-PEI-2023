ad9cac6a1ba37955752f44a216035967a8ffe15a
Pmayaud <paul.mayaud@edu.ece.fr>
2023-12-23 19:53:41 +0100

    modification APID ajout selection plusieur ville + ajout changement de fond

--- web/src/Apid.jsx
+++ web/src/Apid.jsx
@@ -1,35 +1,48 @@
-import "./Apid.css"; 
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Apid.css';
 
 const WeatherWidget = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [selectedPays, setSelectedPays] = useState('France'); // Pays par défaut
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // Liste des pays pour le menu déroulant
+  const pays = ['Republic of France', 'Ethiopia', 'Czech republic', 'Italy', 'Greece','China', 'Poland', 'Thailand', 'Morocco', 'Malaysia','Slovenia', 'Philippines', 'Mexico', 'Ecuador', 'Albania','Japan','Peru','Russia'];
+
+  // Déterminez la classe de fond en fonction de la température
+  const backgroundClass = weatherData && weatherData.main.temp > 20 ? 'weather-widget-chaud' : 'weather-widget-froid';
+
+
   useEffect(() => {
     const fetchWeatherData = async () => {
+      setIsLoading(true);
       try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=e59e9f9354220d18142cf0870207eead&units=metric`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${selectedPays}&appid=e59e9f9354220d18142cf0870207eead&units=metric`);
         setWeatherData(response.data);
+        setError(null);
       } catch (err) {
-        setError(err);
+        setError(err.response && err.response.data.message ? err.response.data.message : err.message);
       } finally {
         setIsLoading(false);
       }
     };
 
     fetchWeatherData();
-  }, []);
+  }, [selectedPays]);
+
+  const handlePaysChange = (event) => {
+    setSelectedPays(event.target.value);
+  };
 
   if (isLoading) {
     return <div className="text-center my-5">Chargement des données météo...</div>;
   }
 
   if (error) {
-    return <div className="text-center text-danger my-5">Erreur lors du chargement des données météo.</div>;
+    return <div className="text-center text-danger my-5">Erreur lors du chargement des données météo : {error}</div>;
   }
 
   if (!weatherData) {
@@ -37,14 +50,21 @@ const WeatherWidget = () => {
   }
 
   return (
-    <div className={`weather-widget-custom d-flex flex-column align-items-center justify-content-center text-white p-3 my-3 rounded shadow`}>
+    <div className={`weather-widget-custom d-flex flex-column align-items-center justify-content-center text-white p-3 my-3 rounded shadow ${backgroundClass}`}>
+      <div className="mb-3 w-100">
+        <select className="form-select custom-select" value={selectedPays} onChange={handlePaysChange}>
+          {pays.map(pays => (
+            <option key={pays} value={pays}>{pays}</option>
+          ))}
+        </select>
+      </div>
       <h2>Météo à {weatherData.name}</h2>
       <p>Température: {weatherData.main.temp} °C</p>
       <p>Humidité: {weatherData.main.humidity}%</p>
-      {/* Ajoutez des boutons ou autres éléments ici, en appliquant des styles personnalisés au besoin */}
-      <button className="custom-button mt-2">Plus d'infos</button>
+      {/*<button className="custom-info-button btn mt-2">Plus d'infos</button>*/}
     </div>
   );
 };
 
 export default WeatherWidget;
+
--- web/src/Apid.stories.jsx
+++ web/src/Apid.stories.jsx
@@ -9,7 +9,55 @@ export default {
 
 const Template = (args) => <WeatherWidget {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  // Insérez ici les props que votre composant attend
+export const Loading = Template.bind({});
+Loading.args = {
+  isLoading: true,
+  weatherData: null,
+  error: null,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  isLoading: false,
+  weatherData: null,
+  error: 'Erreur de chargement des données',
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  isLoading: false,
+  weatherData: {
+    name: 'Paris',
+    main: {
+      temp: 15,
+      humidity: 80
+    }
+  },
+  error: null,
+};
+
+export const WarmWeather = Template.bind({});
+WarmWeather.args = {
+  isLoading: false,
+  weatherData: {
+    name: 'Cairo',
+    main: {
+      temp: 35,
+      humidity: 30
+    }
+  },
+  error: null,
+};
+
+export const CoolWeather = Template.bind({});
+CoolWeather.args = {
+  isLoading: false,
+  weatherData: {
+    name: 'Moscow',
+    main: {
+      temp: 5,
+      humidity: 60
+    }
+  },
+  error: null,
 };
