ff9526f2f976c793f6dc2a7585b6aa1d4a76ef5a
Pmayaud <paul.mayaud@edu.ece.fr>
2023-12-26 20:00:25 +0100

    petite modif

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -4,28 +4,42 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // import App from "./App.jsx";
 
+// Importez vos composants de page
 import Apid from "./Apid.jsx";
 import ApiLocation from "./ApiLocation.jsx";
 import User from "./User.jsx";
 import Sensor_Per_Year from "./Sensor_Per_Year.jsx";
 import ApiAverageTHA from "./ApiAverageTHA.jsx";
+import ApiGraph from "./ApiGraph.jsx";
+
+import OpenCageGeocoder from "./AirQuality.jsx";
+import AdminPage from "./AdminPage"; // Supposons que c'est votre page d'administration
+import Navigation from './Navigation'; // Votre composant de navigation
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-  {/*
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-*/}
-    <Apid />
-    <ApiLocation />
-    <Sensor_Per_Year/>
-    <User/>
-    <ApiAverageTHA/>  
-    
-
-  </>
+  
+  <BrowserRouter>
+    <Navigation /> {}
+    <Routes>
+      <Route path="/" element={
+        <>
+          {}
+          <Apid />
+          <ApiLocation />
+          <Sensor_Per_Year />
+          <User />
+          <ApiAverageTHA />
+          <OpenCageGeocoder />
+          <ApiGraph />
+        </>
+      } />
+      <Route path="/admin" element={<AdminPage />} /> {}
+      {}
+    </Routes>
+  </BrowserRouter>
 );
